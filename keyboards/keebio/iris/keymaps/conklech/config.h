3da4967ea0ecc0b785959b0eef92877c3415055d
Christian Conkle <christian@conkle.org>
2021-05-17 14:41:41 -0700

    Remove I2C since it seems to be broken now on both rev2 and rev4 boards

--- keyboards/keebio/iris/keymaps/conklech/config.h
+++ keyboards/keebio/iris/keymaps/conklech/config.h
@@ -20,7 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Use I2C or Serial, not both */
 
 //#define USE_SERIAL
-#define USE_I2C
+// As of qmk version 0.12.49 I2C support seems to be busted
+// #define USE_I2C
 
 /* Select hand configuration */
 
